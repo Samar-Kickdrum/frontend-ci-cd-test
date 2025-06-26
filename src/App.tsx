c22af7bd83502ba1889da9547f4848ac7c410814
Samar Kumar <samar.kumar@kickdrumtech.com>
2025-06-26 15:53:47 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Backstage</h1>
+      <h1>Backstage- tested ci/cd pipeline</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
